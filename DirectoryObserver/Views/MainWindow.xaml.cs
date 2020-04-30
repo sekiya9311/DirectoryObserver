4df759fde87814b49bf68f9adee295037af017b9
sekiya9311 <sekiya9311@gmail.com>
2020-05-01 01:14:09 +0900

    Impl Notify icon only

--- DirectoryObserver/Views/MainWindow.xaml.cs
+++ DirectoryObserver/Views/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Forms;
 
 namespace DirectoryObserver.Views
 {
@@ -7,9 +12,61 @@ namespace DirectoryObserver.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly NotifyIcon _notifyIcon;
+        private readonly ContextMenuStrip _contextMenu;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _notifyIcon = CreateNotifyIcon();
+            _contextMenu = CreateContextMenu();
+
+            _notifyIcon.ContextMenuStrip = _contextMenu;
+        }
+
+        private NotifyIcon CreateNotifyIcon()
+        {
+            var executeDirPath = Path.GetDirectoryName(
+                Assembly.GetEntryAssembly().Location
+            );
+            var imgPath = Path.Combine(
+                executeDirPath,
+                "Resources",
+                "baseline_folder_open_black_48dp.ico"
+            );
+
+            var res = new NotifyIcon
+            {
+                Visible = true,
+                Text = "DirectoryObserver",
+                Icon = new Icon(imgPath)
+            };
+            return res;
+        }
+
+        private ContextMenuStrip CreateContextMenu()
+        {
+            var exitMenu = new ToolStripMenuItem
+            {
+                Text = "終了"
+            };
+            exitMenu.Click += (s, e) =>
+            {
+                System.Windows.Application.Current.Shutdown();
+            };
+
+            var res = new ContextMenuStrip();
+            res.Items.Add(exitMenu);
+            return res;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _notifyIcon?.Dispose();
+            _contextMenu?.Dispose();
+
+            base.OnClosed(e);
         }
     }
 }
